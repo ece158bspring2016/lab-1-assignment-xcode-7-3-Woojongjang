b9660f0affdd3bbc273322217c06a6f8903ef677
Woo-Jong Jang <woo-jongjang@woo-jongs-mbp.dynamic.ucsd.edu>
2016-04-20 14:59:06 -0700

    Initial Commit

---
+++ ECE158BLab1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ECE158BLab1
+//
+//  Created by Woo-Jong Jang on 4/20/16.
+//  Copyright © 2016 Woo-Jong Jang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
